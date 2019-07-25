437c17d3159c16829c5afb196a8a89752fbbe08c
ityzk <ityzk@qq.com>
2019-07-25 12:31:05 +0800

    增加文件操作

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import ctypes
 import random
+import os
 
+music_path='C:\Users\dengjun\Music\Music'
+'''
 music_list = ["白狐"
     "笨小孩",
     "此生不换",
@@ -15,12 +18,18 @@ music_list = ["白狐"
     "真的爱你",
     "醉美天下"
 ]
+'''
+music_list1=[]
 
-music_name = random.choice(music_list)
+for p in os.listdir(music_path):
+    p = p.split('.')
+    music_list1.append(p[0])
+#print(music_list1)
+#music_name = random.choice(music_list)
+music_name = random.choice(music_list1)
 
 print("正在播放 %s" % music_name)
-
-ctypes.windll.winmm.mciSendStringW(r"open C:\Users\dengjun\Music\Music\%s.mp3 alias s" % music_name, None, 0, None)
-ctypes.windll.winmm.mciSendStringW(r"play s repeat", None, 0, None)
+ctypes.windll.winmm.mciSendStringW(r"open C:\Users\dengjun\Music\Music\%s.mp3 aline s" % music_name, None, 0, None)
+ctypes.windll.winmm.mciSendStringW(r"play s replay" , None, 0, None)
 
 input("\n按回车键退出......")
