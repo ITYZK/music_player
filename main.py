63d3d8e9a717714816f9abc6d85b201c1875f263
ityzk <ityzk@qq.com>
2019-07-25 18:37:28 +0800

    添加了暂停继续停止功能

--- main.py
+++ main.py
@@ -1,35 +1,56 @@
 import ctypes
 import random
 import os
+import pygame
 
-music_path='C:\Users\dengjun\Music\Music'
-'''
-music_list = ["白狐"
-    "笨小孩",
-    "此生不换",
-    "单身情歌",
-    "火苗",
-    "看透爱情看透你",
-    "看月亮爬上来",
-    "来生缘",
-    "郎的诱惑",
-    "天空之城",
-    "我和草原有个约定",
-    "真的爱你",
-    "醉美天下"
-]
-'''
-music_list1=[]
+pygame.mixer.init() 
+class music_player():
+    def load_music(self,music_path):
+        music_list1=[]
+        for p in os.listdir(music_path):
+            p = p.split('.')
+            music_list1.append(p[0])
+        return music_list1
 
-for p in os.listdir(music_path):
-    p = p.split('.')
-    music_list1.append(p[0])
-#print(music_list1)
-#music_name = random.choice(music_list)
-music_name = random.choice(music_list1)
+    def play(self,music_name):
+        
+        print("正在播放 %s\n" % music_name)
+        # ctypes.windll.winmm.mciSendStringW(r"open music\%s.mp3 aline s" % music_name, None, 0, None)
+        # ctypes.windll.winmm.mciSendStringW(r"play s replay" , None, 0, None)
+        pygame.mixer.music.load(r"music\%s.mp3" % music_name)
+        pygame.mixer.music.play()
+    
+    def pause(self):
+        print("已暂停\n")
+        pygame.mixer.music.pause()
 
-print("正在播放 %s" % music_name)
-ctypes.windll.winmm.mciSendStringW(r"open C:\Users\dengjun\Music\Music\%s.mp3 aline s" % music_name, None, 0, None)
-ctypes.windll.winmm.mciSendStringW(r"play s replay" , None, 0, None)
+    def unpause(self):
+        print("继续播放\n")
+        pygame.mixer.music.unpause()
 
-input("\n按回车键退出......")
+    def stop(self):
+        print("已停止播放\n")
+        #ctypes.windll.winmm.mciSendStringW(r"stop s replay" , None, 0, None)
+        pygame.mixer.music.stop()
+
+if __name__ == "__main__":
+    music_path='.\music'
+    m = music_player()
+    m_list = m.load_music(music_path)
+    music_name = random.choice(m_list)
+    m.play(music_name)
+    while(True):
+        ms = input("1.停止 2.切歌 3.暂停 4.继续 0.退出(请输入序号)：")
+        if ms == '1':
+            m.stop()
+        elif ms == '2':
+            music_name = random.choice(m_list)
+            m.play(music_name)
+        elif ms == '3':
+            m.pause()
+        elif ms == '4':
+            m.unpause()
+        elif ms == '0':
+            break
+       
+       
